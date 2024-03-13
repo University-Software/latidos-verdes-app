f02d3cc232412ee0e51126b12fe2a227472f9110
psycotick <minipochis48@gmail.com>
2024-03-13 09:57:53 -0500

    Creación de todos los componentes necesarios hasta el momento para terminar el diseño de la vista número 1, configuración y instalación de boostsrap, variables globales para los colores y manejo de rutas correctamente para cada componente dependiendo el path que se haya asignado, se hace pruebas para indicar que los colores funcionen, librerias y sobre todo la fuente de letra que es Roboto, extraida desde Google Apis Fonts.

---
+++ src/app/suscription-form/suscription-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuscriptionFormComponent } from './suscription-form.component';
+
+describe('SuscriptionFormComponent', () => {
+  let component: SuscriptionFormComponent;
+  let fixture: ComponentFixture<SuscriptionFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SuscriptionFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SuscriptionFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/suscription-form/suscription-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suscription-form',
+  templateUrl: './suscription-form.component.html',
+  styleUrl: './suscription-form.component.css'
+})
+export class SuscriptionFormComponent {
+
+}
